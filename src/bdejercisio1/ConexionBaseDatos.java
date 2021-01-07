82accaddc782b696af62f8d12311167091d178aa
USUARIO <USUARIO@192.168.56.1>
2021-01-07 09:17:55 -0500

    deberes de Henry ordoñez

---
+++ src/bdejercisio1/ConexionBaseDatos.java
@@ -0,0 +1,51 @@
+
+package bdejercisio1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ConexionBaseDatos {
+
+    
+    public static void main(String[] args) {
+        System.out.println("iniciando conexion con la base de datos");
+        ConexionBaseDatos baseDatosMySql = new ConexionBaseDatos ();
+       baseDatosMySql.conexionMysql();
+      
+        
+        
+        
+        
+            
+        }
+    private static final String nombreBaseDatos = "bdEjercisio1";
+    private static final String usuario = "root";
+    private static final String contrasena = "root";
+    private static final String lineaBaseConexion = "jdbc:mysql://localhost:3306/" + nombreBaseDatos + "?useSSL=TRUE";
+    private Connection connect = null;
+    
+    
+    
+    
+        public Connection conexionMysql(){
+            
+        
+        try{
+             connect = DriverManager.getConnection(lineaBaseConexion, usuario, contrasena);
+            
+             if (connect != null) {
+                return connect;
+
+            }
+             
+             
+        }catch (Exception e){
+            System.out.println("Error al conectar:"+ e.getMessage());
+            
+        }
+          return null;      
+        
+    }
+    
+}
